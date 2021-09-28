9eba9f2a4f24bcdb62aa16379481ff92a226f0cb
Joaquín Entrialgo <joaquin@uniovi.es>
2021-09-28 20:28:22 +0200

    Initial structure

---
+++ publi.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+## file: publi.sh
+
+# install tools
+sudo apt-get install -y git make python3 python3-pip python3-setuptools python3-wheel
+
+# setup github config
+git config user.email "joaquin@uniovi.es"
+git config user.name "jentrialgo"
+
+# install dependencies
+sudo pip3 install -r requirements.txt
+
+# pelican commands - install theme put your theme in themes directory
+pelican-themes --install themes/theme-name
+
+# publish to github pages
+ghp-import -m "Generate Pelican site" -b gh-pages output
+git push -f origin gh-pages
